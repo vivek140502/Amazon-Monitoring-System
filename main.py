54bc78dbea120e1c68565ef888219a73aa97e4d3
vivek140502 <136961817+vivek140502@users.noreply.github.com>
2025-03-25 15:38:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -70,6 +70,5 @@ def read_root():
     return {"message": "Hello from FastAPI on Cloud Run"}
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 8080))  # Use PORT 8080
     uvicorn.run(app, host="0.0.0.0", port=port)
